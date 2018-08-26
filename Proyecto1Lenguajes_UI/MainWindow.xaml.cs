4c81166b44305d535a9864d1ee7544632013b0d3
Justin Castillo Valladares <justincastillovalladares@gmail.com>
2018-08-25 19:46:22 -0600

    Cambios importantes en el diseño

--- Proyecto1Lenguajes_UI/MainWindow.xaml.cs
+++ Proyecto1Lenguajes_UI/MainWindow.xaml.cs
@@ -12,17 +12,35 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
 
 namespace Proyecto1Lenguajes_UI
 {
     /// <summary>
     /// Lógica de interacción para MainWindow.xaml
     /// </summary>
+    /// 
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+            //ConnectLib();
         }
+
+        private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^1-9]{1,9}$/");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+
+        /*[DllImport("main.dll", EntryPoint = "resolve_purchase_request")]
+        static extern void resolve_purchase_request(string category, int tickets);
+        public static void ConnectLib()
+        {
+            resolve_purchase_request("Platea", 10);
+        }*/
     }
+
 }
